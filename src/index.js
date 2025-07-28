77ff544c7b549ed28737bdfe3fce07a7d19280cc
Marjukka Niinioja <mniinio@users.noreply.github.com>
2025-07-28 21:03:34 +0300

    Update documentation and example for Rollup build

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ const helpers = require('./helpers');
 const canvasCreator = require('./main');
 const defaultStyles = require('./defaultStyles');
 
+exports.createCanvas = canvasCreator.loadCanvas;
+exports.loadCanvas = canvasCreator.loadCanvas;
+exports.sanitizeInput = helpers.sanitizeInput;
+exports.validateInput = helpers.validateInput;
+exports.distributeMissingPositions = helpers.distributeMissingPositions;
+exports.defaultStyles = defaultStyles;
+
+// default export for CommonJS consumers
 module.exports = {
-  // expose core canvas creation utilities
-  createCanvas: canvasCreator.loadCanvas,
-  loadCanvas: canvasCreator.loadCanvas,
-  sanitizeInput: helpers.sanitizeInput,
-  validateInput: helpers.validateInput,
-  distributeMissingPositions: helpers.distributeMissingPositions,
-  defaultStyles,
+  createCanvas: exports.createCanvas,
+  loadCanvas: exports.loadCanvas,
+  sanitizeInput: exports.sanitizeInput,
+  validateInput: exports.validateInput,
+  distributeMissingPositions: exports.distributeMissingPositions,
+  defaultStyles: exports.defaultStyles,
 };
