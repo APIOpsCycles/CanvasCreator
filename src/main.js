e563e253bfcd528347bb7fdda619a1880e5529fd
Marjukka Niinioja <mniinio@users.noreply.github.com>
2025-08-02 00:08:00 +0300

    minified code and tiny cleanup

--- src/main.js
+++ src/main.js
@@ -472,7 +472,7 @@ fileInput.addEventListener("change", function () {
         .attr("font-size", defaultStyles.fontSize)
         .attr("fill", defaultStyles.fontColor)
         .html(
-          `Template by: ${canvasData.metadata.source} | ${canvasData.metadata.license} | ${canvasData.metadata.authors} | <a href='http://${canvasData.metadata.website}' target='_blank'>${canvasData.metadata.website}</a>`
+          `Template by: ${canvasData.metadata.source} | ${canvasData.metadata.license} | ${canvasData.metadata.authors} | <a href='https://${canvasData.metadata.website}' target='_blank'>${canvasData.metadata.website}</a>`
         )
   
       canvasData.sections.forEach((block, index) => {
@@ -707,7 +707,7 @@ fileInput.addEventListener("change", function () {
           .attr("font-size", defaultStyles.fontSize)
           .attr("fill", defaultStyles.fontColor)
           .html(
-            `Content by: ${contentData?.metadata?.source} | ${contentData?.metadata?.license} | ${contentData?.metadata?.authors} | <a href='http://${contentData?.metadata?.website}' target='_blank'>${contentData?.metadata?.website}</a>`
+            `Content by: ${contentData?.metadata?.source} | ${contentData?.metadata?.license} | ${contentData?.metadata?.authors} | <a href='https://${contentData?.metadata?.website}' target='_blank'>${contentData?.metadata?.website}</a>`
           )
       }
   
